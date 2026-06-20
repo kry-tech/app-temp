b7bbda07ba8195dce372c42d35123b6843bdd0af
kry-tech <anonimomods01@gmail.com>
2026-06-20 11:04:09 -0300

    update

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,11 +2,22 @@ package com.example.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.pm.PackageManager;
+
+import moe.shizuku.api.Shizuku;
 
 public class MainActivity extends Activity {
+
+    int code = 100;
 
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
+
+        if (Shizuku.checkSelfPermission() 
+            != PackageManager.PERMISSION_GRANTED) {
+
+            Shizuku.requestPermission(code);
+        }
     }
 }
