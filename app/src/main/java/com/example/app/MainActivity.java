14056523ade78cc5c980bfaaae44f6d3050252f7
kry-tech <anonimomods01@gmail.com>
2026-06-20 10:58:15 -0300

    add android project

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+    }
+}
