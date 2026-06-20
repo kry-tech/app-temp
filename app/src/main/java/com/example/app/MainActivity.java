0db776338b0e024298649b7871e710106b95bfc8
kry-tech <anonimomods01@gmail.com>
2026-06-20 11:28:03 -0300

    fix shizuku import

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,33 +2,35 @@ package com.example.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.content.pm.PackageManager;
+import android.widget.TextView;
 
 import rikka.shizuku.Shizuku;
 
 public class MainActivity extends Activity {
 
-    int code = 100;
+    private final int REQUEST = 10;
 
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
 
         TextView t = new TextView(this);
-        t.setText("Shizuku teste");
+        t.setText("Abrindo...");
         setContentView(t);
 
-        if (Shizuku.pingBinder()) {
+        if (!Shizuku.pingBinder()) {
+            t.setText("Shizuku não conectado");
+            return;
+        }
 
-            if (Shizuku.checkSelfPermission()
-                    != PackageManager.PERMISSION_GRANTED) {
+        if (Shizuku.checkSelfPermission()
+                != PackageManager.PERMISSION_GRANTED) {
 
-                Shizuku.requestPermission(code);
-            }
+            Shizuku.requestPermission(REQUEST);
 
         } else {
-            t.setText("Shizuku não está iniciado");
+            t.setText("Permissão já dada");
         }
     }
 }
